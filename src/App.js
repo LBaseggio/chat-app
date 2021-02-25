944db2341e5a00dd28834ad970a3d4de8d53e847
Leandro Baseggio <leandrobaseggio@gmail.com>
2021-02-25 20:06:49 +0000

    ChatEngine Initial Configuration - App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import 'Style.css';
+
+
+export default function App() {
+  return (
+    <div>
+      <ChatEngine
+        height='100vh'
+        projectID='8573f163-7af7-41b6-b883-14195ffd904f'
+        userName='Leandro'
+        userSecret='password'
+      />
+    </div>
+  )
+}
+
