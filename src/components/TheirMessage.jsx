db69d367ae59c49ac008af3218715a9094eb9d64
Leandro Baseggio <leandrobaseggio@gmail.com>
2021-02-26 08:41:26 +0000

    Resolver conflicts between ESLINT / PRETTIER 2

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,20 +1,21 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
 export default function TheirMessage({ lastMessage, message }) {
-  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
 
   return (
     <div className="message-row">
       {isFirstMessageByUser && (
-      <div
-        className="message-avatar"
-        style={{
-          backgroundImage:
-                            message.sender && `url(${message.sender.avatar})`,
-        }}
-      />
+        <div
+          className="message-avatar"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
       )}
       {message.attachments && message.attachments.length > 0 ? (
         <img
